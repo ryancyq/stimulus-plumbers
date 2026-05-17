ca71b5d9ea0c92e7205f5024f8e81625fed1699e
Ryan Chang <ryancyq@gmail.com>
2026-05-17 18:06:28 +0800

    test: ad icon coverage for theme

--- stimulus-plumbers-rails/test/stimulus_plumbers/components/icon_test.rb
+++ stimulus-plumbers-rails/test/stimulus_plumbers/components/icon_test.rb
@@ -11,66 +11,6 @@ class IconComponentTest < ActionView::TestCase
     renderer.render(name: name, **kwargs)
   end
 
-  # ── known icon ────────────────────────────────────────────────────────────
-
-  def test_renders_svg_for_known_icon
-    assert_includes render_icon(name: "arrow-left"), "<svg"
-  end
-
-  def test_renders_path_with_d_attribute
-    html = parse_html(render_icon(name: "arrow-left"))
-
-    assert_css html, "svg path[d]"
-  end
-
-  def test_svg_has_viewbox_attribute
-    assert_includes render_icon(name: "arrow-left"), 'viewBox="0 0 24 24"'
-  end
-
-  def test_svg_has_stroke_width_attribute
-    html = parse_html(render_icon(name: "arrow-left"))
-
-    assert_css html, "svg[stroke-width='1.5']"
-  end
-
-  def test_svg_path_has_stroke_linecap
-    html = parse_html(render_icon(name: "arrow-left"))
-
-    assert_css html, "path[stroke-linecap='round']"
-  end
-
-  def test_svg_path_has_stroke_linejoin
-    html = parse_html(render_icon(name: "arrow-left"))
-
-    assert_css html, "path[stroke-linejoin='round']"
-  end
-
-  def test_svg_has_fill_none
-    html = parse_html(render_icon(name: "arrow-left"))
-
-    assert_css html, "svg[fill='none']"
-  end
-
-  def test_svg_has_stroke_current_color
-    html = parse_html(render_icon(name: "arrow-left"))
-
-    assert_css html, "svg[stroke='currentColor']"
-  end
-
-  def test_merges_custom_class
-    html = render_icon(name: "arrow-left", class: "my-icon")
-
-    assert_includes html, "my-icon"
-  end
-
-  def test_passes_html_options
-    html = render_icon(name: "arrow-left", "aria-hidden": "true")
-
-    assert_includes html, 'aria-hidden="true"'
-  end
-
-  # ── unknown icon ──────────────────────────────────────────────────────────
-
   def test_renders_span_for_unknown_icon
     html = render_icon(name: "nonexistent")
 
